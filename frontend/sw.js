46f6b569f97a328d8ba6bee76a01e30e382bb50f
Natwom Daniel <natwomdaniel@gmail.com>
2026-05-22 04:59:24 -0400

    feat: add PWA support - service worker, manifest, installable app

---
+++ frontend/sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'apiaro-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/css/styles.css',
+  '/js/auth.js',
+  '/js/cart.js',
+  '/js/products.js',
+  '/favicon.png',
+  '/apple-touch-icon.png',
+  '/icon-512x512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) return response;
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    })
+  );
+  self.clients.claim();
+});
